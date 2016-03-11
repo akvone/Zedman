398500df8461ee0183889356e9df5643ea5de549
maximkfd <ipust13@gmail.com>
2016-03-11 23:41:31 +0300

    I don't know why this commit exist, but it does.

---
+++ core/src/com/kivi/zedman/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.kivi.zedman.utils;
+
+/**
+ * Class for global constants.
+ *
+ */
+public class Constants {
+    public static final float PPM = 32; //Point Per Meter - converting from Box2d to render coordinates
+}
