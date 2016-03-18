e545a07a361acfeb017ce9b10a4eb3a5e92a0824
maximkfd <ipust13@gmail.com>
2016-03-18 14:56:33 +0300

    Server interaction(local)

--- core/src/com/kivi/zedman/system/Zedman.java
+++ core/src/com/kivi/zedman/system/Zedman.java
@@ -1,19 +1,7 @@
 package com.kivi.zedman.system;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Vector2;
 import com.kivi.zedman.screens.GameScreen;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.net.URISyntaxException;
-
-import io.socket.client.IO;
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
 
 /**
  * Created by Kirill on 04.03.2016.
